e5879147e4481330dc37741cb6f585891d785940
Nader <naderamama2022@gmail.com>
2025-11-24 23:26:40 +0100

    new feature: rewrite frontend with Angular

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,15 @@
-import { bootstrapApplication } from "@angular/platform-browser"
-import { provideRouter } from "@angular/router"
-import { provideHttpClient, HTTP_INTERCEPTORS } from "@angular/common/http"
-import { provideAnimations } from "@angular/platform-browser/animations"
-import { AppComponent } from "./app/app.component"
-import { routes } from "./app/app.routes"
-import { HttpConfigInterceptor } from "./app/core/interceptors/http.interceptor"
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app/app.component";
+import { routes } from "./app/app.routes";
+import { httpInterceptor } from "./app/core/interceptors/http.interceptor";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
-    { provide: HTTP_INTERCEPTORS, useClass: HttpConfigInterceptor, multi: true },
+    provideHttpClient(withInterceptors([httpInterceptor])),
     provideAnimations(),
   ],
-}).catch((err) => console.error(err))
+}).catch((err) => console.error(err));
